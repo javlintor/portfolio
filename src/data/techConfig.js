c6b7978d08d20ca30ea1839ac101a72ac837e4a7
javlintor <jlinarestorres2@gmail.com>
2025-11-08 19:54:53 +0100

    favicon and tech badge

---
+++ src/data/techConfig.js
@@ -0,0 +1,36 @@
+export const BadgeConfig = {
+  nvim: {
+    label: 'Neovim',
+    colors: {
+      background: '#1abc9c20',
+      text: '#1abc9c',
+      border: '#1abc9c',
+    },
+  },
+  lua: {
+    label: 'Lua',
+    colors: {
+      background: '#ff5d0120',
+      text: '#ff5d01',
+      border: '#ff5d01',
+    },
+  },
+  react: {
+    label: 'React',
+    icon: '/assets/tech/react.svg',
+    colors: {
+      background: '#61dafb20',
+      text: '#61dafb',
+      border: '#61dafb',
+    },
+  },
+  tailwind: {
+    label: 'TailwindCSS',
+    icon: '/assets/tech/tailwind.svg',
+    colors: {
+      background: '#38bdf820',
+      text: '#38bdf8',
+      border: '#38bdf8',
+    },
+  },
+}
